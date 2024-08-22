b695acc64a19c9c2d9bbdb37dba8fe1d82d0cf96
Ali Sepahvand <alih.sepahvand@gmail.com>
2024-08-22 05:12:08 -0600

    fixed the grid and also made a simple spice solver

--- elmer_solver.py
+++ elmer_solver.py
@@ -13,15 +13,16 @@ writeDataName = "Potential"
 
 dimensions = interface.get_mesh_dimensions(meshName)
 
-N = 5
-L = 5
+N = 1
 # Define mesh coordinates and register coordinates
-grid = np.zeros([N + 1, dimensions])
-grid[:, 0] = np.linspace(0, L, N + 1)  # x component
+grid = np.zeros([N , dimensions])
+grid[:, 0] = np.linspace(0, N-1, N )  # x component
 grid[:, 1] = 0  # np.linspace(0, config.L, N+1)  # y component, leave blank
 
 pad_potentials = [650]
-writeData = pad_potentials[0] * np.ones(N + 1)
+pad_areas = [1e-4]
+
+writeData = pad_potentials * np.ones(N)
 vertexIDs = interface.set_mesh_vertices(meshName, grid)
 
 if interface.requires_initial_data():
@@ -43,6 +44,16 @@ while interface.is_coupling_ongoing():
     # Read data, solve timestep, and write data
     pad_current_densities = interface.read_data(
         meshName, readDataName, vertexIDs, precice_dt)
+    print('-------------read current densities')
+    print('printing currents')
+    print(pad_current_densities*pad_areas)
+    # print(pad_current_densities)
+    pad_currents = pad_current_densities * pad_areas
+
+    # calculate the trace resistance
+    trace_resistance = 1e3
+    writeData = [650 - pad_currents[0] * trace_resistance]
+
     interface.write_data(meshName, writeDataName,
                          vertexIDs, writeData)
     
--- spice_solver.py
+++ spice_solver.py
@@ -13,16 +13,19 @@ writeDataName = "Current Density"
 
 dimensions = interface.get_mesh_dimensions(meshName)
 
-N = 5
-L = 5
+N = 1
 # Define mesh coordinates and register coordinates
-grid = np.zeros([N + 1, dimensions])
-grid[:, 0] = np.linspace(0, L, N + 1)  # x component
+grid = np.zeros([N , dimensions])
+grid[:, 0] = np.linspace(0, N-1, N )  # x component
 grid[:, 1] = 0  # np.linspace(0, config.L, N+1)  # y component, leave blank
 
-pad_currents = [10]
+Vin = 650
+R1 = 1e3
+trace_resistance = 1e-3
+pad_currents = [Vin/(R1+trace_resistance)]
 pad_areas = [1e-4]
-writeData = pad_currents[0]/pad_areas[0] * np.ones(N + 1)
+pad_current_density = np.array(pad_currents)/np.array(pad_areas)
+writeData = pad_current_density * np.ones(N)
 vertexIDs = interface.set_mesh_vertices(meshName, grid)
 
 if interface.requires_initial_data():
@@ -34,16 +37,26 @@ print("Spice: init precice...")
 interface.initialize()
 
 pad_potentials = interface.read_data(meshName, readDataName, vertexIDs, 0)
-
+iter = 0
 while interface.is_coupling_ongoing():
+    iter += 1
     # When an implicit coupling scheme is used, checkpointing is required
     if interface.requires_writing_checkpoint():
         pass
     precice_dt = interface.get_max_time_step_size()
 
+
     # Read data, solve timestep, and write data
     pad_potentials = interface.read_data(
         meshName, readDataName, vertexIDs, precice_dt)
+    print('-------------read potentials')
+    print(pad_potentials)
+    
+    # calculate the trace resistance
+    trace_resistance = (650 - pad_potentials[0]) / pad_currents[0]
+    pad_currents = [Vin/(R1 + trace_resistance)]
+    pad_current_density = np.array(pad_currents)/np.array(pad_areas)
+    writeData = pad_current_density * np.ones(N)
     interface.write_data(meshName, writeDataName,
                          vertexIDs, writeData)
 
